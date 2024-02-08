14a57655212b47a0ad6d4c80f999b6805cb21582
Pranav Goyal <pr945005@dal.ca>
2024-02-08 14:51:49 +0530

    [Pranav] Add : initializing project

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace SnakeAndLadder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welocme to Snake Ladder Problem");
+    }
+}
+
